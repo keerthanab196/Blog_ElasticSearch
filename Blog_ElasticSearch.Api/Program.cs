d279d3c651cba2b658151b41a7449373c7a1e70e
keerthanab196 <keerthanab196@gmail.com>
2025-04-23 16:18:16 +0100

    Added the nuget packges - Nest and Elasticsearch.Net in the project Blog_ElasticSearch.Api Elastic search configurations settings has been set up in program.cs

--- Blog_ElasticSearch.Api/Program.cs
+++ Blog_ElasticSearch.Api/Program.cs
@@ -1,4 +1,13 @@
+
+using Nest;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var elasticSettings = new ConnectionSettings(new Uri("http://localhost:9200"))
+    .DefaultIndex("BlogArticles");
+var client = new ElasticClient(elasticSettings);
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
