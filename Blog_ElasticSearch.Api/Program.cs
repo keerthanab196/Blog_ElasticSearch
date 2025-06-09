5dd9d43f72da371dc340b440d2c49120d657f024
keerthanab196 <keerthanab196@gmail.com>
2025-06-09 13:33:52 +0100

    Registered the serviices for DI

--- Blog_ElasticSearch.Api/Program.cs
+++ Blog_ElasticSearch.Api/Program.cs
@@ -1,5 +1,10 @@
 
+using Blog_ElasticSearch.Infrastructure.Interfaces;
+using Blog_ElasticSearch.Infrastructure.Services;
+using Blog_ElasticSearch.Application.Interfaces;
+using Blog_ElasticSearch.Application.Services;
 using Nest;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +12,9 @@ var elasticSettings = new ConnectionSettings(new Uri("http://localhost:9200"))
     .DefaultIndex("BlogArticles");
 var client = new ElasticClient(elasticSettings);
 
+builder.Services.AddSingleton<IElasticClient>(client);
+builder.Services.AddScoped<IArticleRepository, ArticleRepository>();
+builder.Services.AddScoped<IArticleService, ArticleService>();
 
 var app = builder.Build();
 
