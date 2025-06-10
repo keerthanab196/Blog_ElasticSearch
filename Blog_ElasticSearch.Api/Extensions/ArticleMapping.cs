e0395d930df183ce7e41c231e41cac70146d28e2
keerthanab196 <keerthanab196@gmail.com>
2025-06-10 16:34:06 +0100

    Added and extension class ArticleMapping to help in mapping between Article and ArticleResponseDTO

---
+++ Blog_ElasticSearch.Api/Extensions/ArticleMapping.cs
@@ -0,0 +1,17 @@
+﻿using Blog_ElasticSearch.Api.DTOs;
+using Blog_ElasticSearch.Domain.Entities;
+using System.Runtime.CompilerServices;
+
+namespace Blog_ElasticSearch.Api.Extensions
+{
+    public static class ArticleMapping
+    {
+        public static ArticleResponseDTO MapToArticleResponseDTO(this Article a) => new()
+        {
+            Title = a.Title,
+            Content = a.Content,
+            PublishedDate = a.PublishedDate,
+            Author=a.Author
+        };
+    }
+}
