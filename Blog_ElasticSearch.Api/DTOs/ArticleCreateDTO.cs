92593f1348e802515d96559e5f5aea4dbbe0664e
keerthanab196 <keerthanab196@gmail.com>
2025-06-10 16:21:36 +0100

    Created DTOs for data transfer between API and external client.

---
+++ Blog_ElasticSearch.Api/DTOs/ArticleCreateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog_ElasticSearch.Api.DTOs
+{
+    public class ArticleCreateDTO
+    {
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Content { get; set; } = string.Empty;
+        [Required]
+        public string Author { get; set; } = string.Empty;
+    }
+}
---
+++ Blog_ElasticSearch.Api/DTOs/ArticleResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog_ElasticSearch.Api.DTOs
+{
+    public class ArticleResponseDTO
+    {
+        public string? Title { get; set; } 
+        public string? Content { get; set; } 
+        public DateTime? PublishedDate { get; set; }
+        public string? Author { get; set; }
+    }
+}
